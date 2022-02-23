ba5c68194ae18e1d14d7248543c6139ed5ee2f06
ohjiyeon3275 <ojy940716@gmail.com>
2022-02-23 23:26:05 +0900

    init

---
+++ src/main/kotlin/com/jiyeon/kotlinproject/HelloController.kt
@@ -0,0 +1,21 @@
+package com.jiyeon.kotlinproject
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/hello")
+class HelloController {
+
+    @GetMapping("/jiyeon")
+    fun helloJiyeon(): String{
+        return "Hello, this is jiyeon :D "
+    }
+
+    // also ok
+    @GetMapping("/world")
+    fun helloWorld(): String = "Hello this is world :D"
+
+
+}
---
+++ src/test/kotlin/com/jiyeon/kotlinproject/KotlinprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jiyeon.kotlinproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
