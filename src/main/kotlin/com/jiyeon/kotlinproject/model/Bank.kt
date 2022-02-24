634aedcf76f23594a5d2e07191197e3ce3c32faf
ohjiyeon3275 <ojy940716@gmail.com>
2022-02-24 21:13:55 +0900

    dataclass simple

--- src/main/kotlin/com/jiyeon/kotlinproject/model/Bank.kt
+++ src/main/kotlin/com/jiyeon/kotlinproject/model/Bank.kt
@@ -4,30 +4,4 @@ data class Bank (
     val accountNumber: String,
     val trust: Double,
     val transactionFee: Int
-){
-
-    override fun toString(): String {
-        return "Bank(accountNumber='$accountNumber', trust=$trust, transactionFee=$transactionFee)"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Bank
-
-        if (accountNumber != other.accountNumber) return false
-        if (trust != other.trust) return false
-        if (transactionFee != other.transactionFee) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = accountNumber.hashCode()
-        result = 31 * result + trust.hashCode()
-        result = 31 * result + transactionFee
-        return result
-    }
-
-}
+)
---
+++ src/test/kotlin/com/jiyeon/kotlinproject/datasource/mock/MockBackDataSourceTest.kt
@@ -0,0 +1,5 @@
+package com.jiyeon.kotlinproject.datasource.mock
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MockBackDataSourceTest
