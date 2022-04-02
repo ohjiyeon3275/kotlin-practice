06d243d4db078394a6e43288b8ab4d359697ef9f
ohjiyeon3275 <ojy940716@gmail.com>
2022-04-02 20:54:47 +0900

    ㅋㄹㅌ

---
+++ src/main/kotlin/com/jiyeon/kotlinproject/crt/GlobalScope.kt
@@ -0,0 +1,42 @@
+package com.jiyeon.kotlinproject.crt
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+
+fun now() = ZonedDateTime.now().toLocalTime().truncatedTo(ChronoUnit.MILLIS)
+
+fun log(msg:String) = println("${now()}:${Thread.currentThread()}:${msg}")
+
+fun globalScope(){
+    GlobalScope.launch {
+        log("coroutine started")
+    }
+}
+
+fun main() {
+    log(">>>>> main")
+    globalScope()
+    log(">>>>> globalScope ")
+//    Thread.sleep(5000L)
+//    runBlockingExecute()
+    yieldRunBlocking()
+    log(">>>>> main ended")
+
+    /**
+     * log>>
+    20:35:14.570:Thread[main,5,main]:>>>>> main
+    20:35:14.715:Thread[main,5,main]:>>>>> globalScope
+    20:35:14.735:Thread[DefaultDispatcher-worker-1,5,main]:coroutine started
+    20:35:19.722:Thread[main,5,main]:>>>>> main ended
+     *
+     * things to be aware
+     *
+     * 1. coroutine thread is different from main thread
+     * 2. if main() ended before coroutine executed, the coroutine one is never run
+     *    because main() is on 'Synchronized'
+     *  2-1. to prevent 2 happens, added Thread.sleep(5000L) or runBlocking
+     *  2-2. or use Asynchronized : CoroutineScope
+     */
+}
---
+++ src/main/kotlin/com/jiyeon/kotlinproject/crt/RunBlocking.kt
@@ -0,0 +1,69 @@
+package com.jiyeon.kotlinproject.crt
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+fun runBlockingExecute() {
+    runBlocking {
+        launch {
+            log("runBlocking executed")
+        }
+    }
+}
+
+/**
+ *
+20:46:48.209:Thread[main,5,main]:>>>>> main
+20:46:48.340:Thread[main,5,main]:>>>>> globalScope
+20:46:48.361:Thread[DefaultDispatcher-worker-1,5,main]:coroutine started
+20:46:48.374:Thread[main,5,main]:global launch started
+20:46:48.375:Thread[main,5,main]:>>>>> main ended
+ */
+
+
+fun yieldRunBlocking() {
+    runBlocking {
+        launch {
+            log("1>>")
+            yield()
+            log("3>>>")
+            yield()
+            log("5>>>")
+        }
+
+        log("after 1st launch run")
+
+        launch {
+            log("2")
+            delay(1000L)
+            log("4")
+            delay(1000L)
+            log("6")
+        }
+
+        log("after 2nd launch run")
+
+    }
+}
+
+/**
+ *
+20:51:47.414:Thread[main,5,main]:>>>>> main
+20:51:47.545:Thread[main,5,main]:>>>>> globalScope
+20:51:47.566:Thread[DefaultDispatcher-worker-2,5,main]:coroutine started
+20:51:47.571:Thread[main,5,main]:after 1st launch run
+20:51:47.578:Thread[main,5,main]:after 2nd launch run
+20:51:47.578:Thread[main,5,main]:1>>
+20:51:47.579:Thread[main,5,main]:2
+20:51:47.586:Thread[main,5,main]:3>>>
+20:51:47.586:Thread[main,5,main]:5>>>
+20:51:48.588:Thread[main,5,main]:4
+20:51:49.594:Thread[main,5,main]:6
+20:51:49.596:Thread[main,5,main]:>>>>> main ended
+
+ which means :  1. launch -> instantly returned
+                2. runBlocking is the last one executed
+                3. ...... the order of delay and yield.
+ */
